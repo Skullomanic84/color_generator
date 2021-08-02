13401b057755f58098e30b70fb69df1997109d0a
Skullomanic84 <jhshiloh@gmail.com>
2021-08-02 18:00:22 +0200

    javascript color generator

---
+++ app.js
@@ -0,0 +1,23 @@
+let css = document.querySelector ('h3');
+let color1= document.querySelector ('.color1');
+let color2= document.querySelector ('.color2');
+let body = document.getElementById ('gradient');
+
+
+function setGradient() {
+    body.style.background = 
+    'linear-gradient(to right, '
+     + color1.value + 
+     ',' 
+     + color2.value 
+     + ')';
+
+     css.textContent = body.style.background + ';';
+}
+//attaching event listener to color picker
+
+color1.addEventListener('input', setGradient);
+
+color2.addEventListener('input', setGradient);
+
+
